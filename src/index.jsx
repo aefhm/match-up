7ccf9389276da5e164255811e28a13ff186760f4
Xi Zhang <xizhang11@gmail.com>
2018-03-25 13:14:52 -0700

    Add prop types

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
+import PropTypes from 'prop-types';
 
 
 const cardLimit = 52;
@@ -23,19 +23,29 @@ const store = createStore(function (state = {}, action) {
 class Card extends React.Component {
   render() {
     return (
-      <div className="card">
+      <div className="card" onClick={this.props.click}>
         {this.props.value + ' of ' + this.props.suit}
       </div>
     );
   }
 }
 
+Card.propTypes = {
+  click: PropTypes.func,
+};
+
 class Board extends React.Component {
   render() {
     let cards = [];
 
     for (let i = 0; i < cardLimit; i++)
-      cards.push(<Card key={i} index={i} value={ i % 13 } suit={ suitLabel[Math.floor(i / 13)] } />);
+      cards.push(<Card
+        key={i}
+        index={i}
+        value={i % 13}
+        suit={suitLabel[Math.floor(i / 13)]}
+        click={function () { console.log('cilkced'); }}
+      />);
 
     cards = shuffle(cards);
 
@@ -52,7 +62,7 @@ class Stats extends React.Component {
     let cards = [];
 
     for (let i = 0; i < this.props.matchedCards; i++)
-      cards.push(<Card key={i} index={i} value={ i % 13 } suit={ suitLabel[Math.floor(i / 13)] } />);
+      cards.push(<Card key={i} index={i} value={i % 13} suit={suitLabel[Math.floor(i / 13)]} />);
 
     return (
       <div className="stats">
