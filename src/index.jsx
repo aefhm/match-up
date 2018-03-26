e8a00f23cd6c4931a2270b0ce8fe746e201aaacd
Xi Zhang <xizhang11@gmail.com>
2018-03-25 18:50:40 -0700

    Add redux logging

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import Game from './game';
 
-const store = createStore(function (state = {}, action) {
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+const initialState = {
+  matchedCards: [],
+  turnedCards: [],
+};
+
+const reducer = function (state = initialState, action) {
   switch (action.type) {
     case 'TURN_CARD':
-      return Object.assign({}, state, { turnedCards: state['turnedCards'] + 1 });
+      return Object.assign({}, state, { turnedCards: [...state['turnedCards'], action.payload] });
+    case 'MATCHED_CARDS':
+      return Object.assign({}, state, {
+        turnedCards: [],
+        matchedCards: [...state['matchedCards'], ...action.payload]
+      });
+    case 'CLEAR_TURNED_CARDS':
+      return Object.assign({}, state, { turnedCards: [] });
     default:
       return state;
   }
-});
+};
+
+const store = createStore(reducer, initialState, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
